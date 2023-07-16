7c370bd54cea8d1c9599febc7a8308447f628a84
sophia ojegba <roseysofia@yahoo.com>
2023-07-16 04:05:56 -0400

    Added code to find words vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+        if (l.includes(word)){ 
+            return true
+        }
+                
+    }  
+
+    const verticalJoin = letters[0].map((_, index) => letters.map(row => row[index]).join(''));
+    for (s of verticalJoin) {
+        if (s.includes(word)){ 
+            return true
+        }
+                
+    }  return false;
+
 }
 
 module.exports = wordSearch
+
+//Paired Alone this week as my partner has dropped from the program. Completed the word search for 1 hour and alot of google search. 
